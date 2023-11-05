c1d8fc27fa6e503f95686f3a3e43be34411f416b
Sunil Bombe <sunil.bombe1998@gmail.com>
2023-11-05 18:47:15 +0530

    added changes

---
+++ currency-conversion-service/src/main/java/com/webwork/currencyconversionservice/beans/CurrencyConversionBeans.java
@@ -0,0 +1,19 @@
+package com.webwork.currencyconversionservice.beans;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBeans {
+
+	private int id;
+	
+	private String from;
+	
+	private String to;
+	
+	private BigDecimal multipleConversion;
+	
+	private BigDecimal quantity;
+	
+	private BigDecimal totalCalculatedAmount;
+	
+}
